09949ac933e996fda06ef36b3a525113f269789a
Megan_Comeau <mlcomeau99@gmail.com>
2020-09-02 16:08:50 -0700

    added material-ui components for style

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,19 @@
 import React from 'react';
 import Logout from './Logout.js';
+import AppBar from '@material-ui/core/AppBar';
+import Typography from '@material-ui/core/Typography';
+import Toolbar from '@material-ui/core/Toolbar';
 
 const Home = () => {
 
     return (
         <div>
+            <AppBar position="fixed">
+                <Toolbar>
+                    <Typography variant="h6">AdoptABuddy</Typography>
+                
+                </Toolbar>
+            </AppBar>
             <p>This is where the user goes when they are logged in </p>
             <Logout />
         </div>
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,10 +1,43 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { Link } from 'react-router-dom';
+//import { Link } from 'react-router-dom';
 import { updateLoginForm } from '../actions/loginForm.js';
-import { login } from '../actions/currentUser.js'
+import { login } from '../actions/currentUser.js';
+import Avatar from '@material-ui/core/Avatar';
+import Button from '@material-ui/core/Button';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import TextField from '@material-ui/core/TextField';
+import Link from '@material-ui/core/Link';
+import Grid from '@material-ui/core/Grid';
+import Box from '@material-ui/core/Box';
+import PetsIcon from '@material-ui/icons/Pets';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
+
+const useStyles = makeStyles((theme) => ({
+    paper: {
+      marginTop: theme.spacing(8),
+      display: 'flex',
+      flexDirection: 'column',
+      alignItems: 'center',
+    },
+    avatar: {
+      margin: theme.spacing(1),
+      backgroundColor: theme.palette.secondary.main,
+    },
+    form: {
+      width: '100%', // Fix IE 11 issue.
+      marginTop: theme.spacing(1),
+    },
+    submit: {
+      margin: theme.spacing(3, 0, 2),
+    },
+  }));
 
 const Login = ({ loginForm, updateLoginForm, login }) => {
+
+    const classes = useStyles();
     
     const handleInputChange = event => {
         const { name, value } = event.target
@@ -20,17 +53,67 @@ const Login = ({ loginForm, updateLoginForm, login }) => {
     }
 
     return (
-        <div className="login">
-            <h1>Welcome to AdoptABuddy</h1>
-            <form onSubmit={handleSubmit}>
-                <input type="text" name="email" value={loginForm.username} placeholder="email" onChange={handleInputChange}/><br/>
-                <input type="password" name="password" value={loginForm.password} placeholder="password" onChange={handleInputChange}/><br/>
-                <input type="submit" value="Login"/><br/>
-            </form><br/>
-            <p>Not a user yet?</p>
-            <Link to="/signup">Signup Now!</Link>
+      <Container component="main" maxWidth="xs">
+        <CssBaseline />
+        <div className={classes.paper} onSubmit={handleSubmit}>
+          <Avatar className={classes.avatar}>
+            <PetsIcon />
+          </Avatar>
+          <Typography component="h1" variant="h5">
+            Welcome to AdoptABuddy
+          </Typography>
+          <Typography variant="subtitle2">
+            Please sign in.
+          </Typography>
+          <form className={classes.form} noValidate>
+            <TextField
+              value={loginForm.username}
+              onChange={handleInputChange}
+              variant="outlined"
+              margin="normal"
+              required
+              fullWidth
+              id="email"
+              label="Email Address"
+              name="email"
+              autoComplete="email"
+              autoFocus
+            />
+            <TextField
+              value={loginForm.password}
+              onChange={handleInputChange}
+              variant="outlined"
+              margin="normal"
+              required
+              fullWidth
+              name="password"
+              label="Password"
+              type="password"
+              id="password"
+              autoComplete="current-password"
+            />
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              color="primary"
+              className={classes.submit}
+            >
+              Sign In
+            </Button>
+            <Grid container>
+              <Grid item>
+                <Link href="/signup" variant="body2">
+                  {"Don't have an account? Sign Up"}
+                </Link>
+              </Grid>
+            </Grid>
+          </form>
         </div>
-    )
+      </Container>
+    );
+
+
 }
 
 const mapStateToProps = state => {
